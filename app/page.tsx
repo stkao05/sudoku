6a56455e7065f737c3c0c7e65fcadf43fd699585
Steven Kao <st.kao.05@gmail.com>
2024-07-15 16:20:14 +0800

    difficulty

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,46 @@
 import { createClient } from "@/services/supabase";
 import Link from "next/link";
+import clsx from "clsx";
 
 export default async function Home() {
   const supabase = createClient();
-  const { data: puzzles } = await supabase.from("sudoku_puzzles").select("id");
+  const { data: puzzles } = await supabase
+    .from("sudoku_puzzles")
+    .select("id,difficulty");
+
   if (!puzzles) {
     return <div>no data</div>;
   }
+
+  const level = {
+    1: "easy",
+    2: "medium",
+    3: "hard",
+  };
 
   return (
     <>
       <div className="mb-2">Choose game</div>
       <div className="border border-neutral-200">
         {puzzles.map((p) => {
+          const lvCls = {};
+
           return (
             <Link
-              className="block border-b border-neutral-200 last:border-none p-2 hover:bg-neutral-100"
+              className="flex justify-between border-b border-neutral-200 last:border-none p-2 hover:bg-neutral-100"
               key={p.id}
               href={`/game/${p.id}`}
             >
-              {p.id.substring(0, 8)}
+              <div>{p.id.substring(0, 8)}</div>
+              <div
+                className={clsx({
+                  "text-green-500": p.difficulty === 1,
+                  "text-yellow-500": p.difficulty === 2,
+                  "text-red-500": p.difficulty === 3,
+                })}
+              >
+                {p.difficulty && level[p.difficulty as 1 | 2 | 3]}
+              </div>
             </Link>
           );
         })}
--- types/database.ts
+++ types/database.ts
@@ -4,46 +4,49 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   public: {
     Tables: {
       sudoku_puzzles: {
         Row: {
-          created_at: string;
-          id: string;
-          puzzle: string;
-        };
+          created_at: string
+          difficulty: number | null
+          id: string
+          puzzle: string
+        }
         Insert: {
-          created_at?: string;
-          id?: string;
-          puzzle: string;
-        };
+          created_at?: string
+          difficulty?: number | null
+          id?: string
+          puzzle: string
+        }
         Update: {
-          created_at?: string;
-          id?: string;
-          puzzle?: string;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string
+          difficulty?: number | null
+          id?: string
+          puzzle?: string
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type PublicSchema = Database[Extract<keyof Database, "public">];
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -52,23 +55,23 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-      PublicSchema["Views"])
-  ? (PublicSchema["Tables"] &
-      PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R;
-    }
-    ? R
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -76,20 +79,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I;
-    }
-    ? I
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -97,20 +100,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U;
-    }
-    ? U
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -118,9 +121,9 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-  : never;
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
