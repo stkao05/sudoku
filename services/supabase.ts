d14549ecb022cc771b79fa95322b2dac09947354
Steven Kao <st.kao.05@gmail.com>
2024-07-15 23:40:59 +0800

    doc

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,9 +1,21 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { Database } from "@/types/database";
 
 export const createClient = () => {
-  return createBrowserClient<Database>(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      // intentionally not implement cookie access, as we
+      // don't need it now and we want to use the client
+      // during static rendering (cookie access isn't allowed
+      // during static rendering)
+      cookies: {
+        getAll() {
+          return [];
+        },
+        setAll(cookiesToSet) {},
+      },
+    }
   );
 };
