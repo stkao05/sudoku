1220d349fcc4e80b15c301c7237969ecd6b1b082
Steven Kao <st.kao.05@gmail.com>
2024-07-15 23:34:15 +0800

    db

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,24 +1,9 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "@/types/database";
 
 export const createClient = () => {
-  // const cookieStore = cookies();
-  return createServerClient<Database>(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          // return cookieStore.getAll();
-          return [];
-        },
-        setAll(values) {
-          // values.forEach(({ name, value, options }) => {
-          //   cookieStore.set({ name, value, ...options });
-          // });
-        },
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 };
