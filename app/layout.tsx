7d4c0f421ecc924043e3f6807557768878b8d652
Steven Kao <st.kao.05@gmail.com>
2024-07-15 22:59:06 +0800

    responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-mono">
         <main className="flex flex-col items-center justify-center bg-neutral-100">
-          <div className="w-[600px] h-screen px-6 pt-4 bg-white">
+          <div className="w-full sm:w-[600px] h-screen px-6 pt-4 bg-white">
             <h1 className="font-medium text-xl mb-4">
               <Link href="/">sudoko</Link>
             </h1>
